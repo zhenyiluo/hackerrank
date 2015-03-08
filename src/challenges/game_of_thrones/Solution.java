adafb49c6a52c8bf97dff8de6d616908a4c00655
Zhenyi Luo <zhenyiluo@gmail.com>
2015-03-08 19:34:19 -0400

    Solve warm up questions

---
+++ src/challenges/game_of_thrones/Solution.java
@@ -0,0 +1,29 @@
+package challenges.game_of_thrones;
+
+/**
+ * Created by Eric on 15-3-8.
+ */
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        System.out.println(solve(s));
+    }
+    public static String solve(String s){
+        int len = s.length();
+        int mask = 0;
+        for(int i = 0; i < len; i++){
+            mask ^= (1 << (s.charAt(i) - 'a'));
+        }
+        if(Integer.bitCount(mask) <=1){
+            return "YES";
+        }else{
+            return "NO";
+        }
+    }
+}
---
+++ src/challenges/the_love_letter_mystery/Solution.java
@@ -0,0 +1,29 @@
+package challenges.the_love_letter_mystery;
+
+/**
+ * Created by Eric on 15-3-8.
+ */
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for(int i = 0; i < T; i++){
+            String s = sc.next();
+            int result = solve(s);
+            System.out.println(result);
+        }
+    }
+    public static int solve(String s){
+        int len = s.length();
+        int result = 0;
+        for(int i = 0; i < len/2; i++){
+            result += Math.abs(s.charAt(i) - s.charAt(len - i -1));
+        }
+        return result;
+    }
+}
