2051a153a95c5a7667787c01b2932d9c32e72e30
Zhenyi Luo <zhenyiluo@gmail.com>
2015-03-08 18:12:22 -0400

    Even tree is interesting

---
+++ src/challenges/minimum_draws/Solution.java
@@ -0,0 +1,20 @@
+package challenges.minimum_draws;
+
+/**
+ * Created by Eric on 15-3-8.
+ */
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for(int i = 0; i < T; i++){
+            int N = sc.nextInt();
+            System.out.println(N+1);
+        }
+    }
+}
