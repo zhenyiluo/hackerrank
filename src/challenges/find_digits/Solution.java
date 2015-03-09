5601469a8526984494a26c72c9223c1f290952c0
Zhenyi Luo <zhenyiluo@gmail.com>
2015-03-08 23:28:39 -0400

    10714

---
+++ src/challenges/find_digits/Solution.java
@@ -0,0 +1,33 @@
+package challenges.find_digits;
+
+/**
+ * Created by Eric on 15-3-8.
+ */
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for(int i = 0; i < T; i++){
+            long N = sc.nextLong();
+            int result = solve(N);
+            System.out.println(result);
+        }
+    }
+    public static int solve(long N){
+        int result = 0;
+        long tmp = N;
+        while(tmp > 0){
+            long digit = tmp % 10;
+            if(digit > 0 && N % digit == 0){
+                result ++;
+            }
+            tmp /= 10;
+        }
+        return result;
+    }
+}
