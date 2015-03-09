fe04f1d0c0d60f84210a0212a37acc9ccba3e0f2
Zhenyi Luo <zhenyiluo@gmail.com>
2015-03-08 23:35:48 -0400

    10085

---
+++ src/challenges/angry_professor/Solution.java
@@ -0,0 +1,31 @@
+package challenges.angry_professor;
+
+/**
+ * Created by Eric on 15-3-8.
+ */
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for(int i = 0; i < T; i++){
+            int N = sc.nextInt();
+            int K = sc.nextInt();
+            int count = 0;
+            for(int j = 0; j < N; j++){
+                if(sc.nextInt() <= 0){
+                    count++;
+                }
+            }
+            if(count >= K){
+                System.out.println("NO");
+            }else{
+                System.out.println("YES");
+            }
+        }
+    }
+}
