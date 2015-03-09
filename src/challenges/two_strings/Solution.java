bd0f4955222bd96b2f13149901cc2bd63e9ab6ac
Zhenyi Luo <zhenyiluo@gmail.com>
2015-03-08 23:45:17 -0400

    9167

---
+++ src/challenges/two_strings/Solution.java
@@ -0,0 +1,32 @@
+package challenges.two_strings;
+
+/**
+ * Created by Eric on 15-3-8.
+ */
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for(int i = 0; i < T; i++){
+            String A = sc.next();
+            String B = sc.next();
+            boolean flag = false;
+            for(char c = 'a'; c <= 'z' && !flag; c++){
+                if(A.indexOf(c) != -1 && B.indexOf(c) != -1){
+                    flag = true;
+                }
+            }
+            if(flag){
+                System.out.println("YES");
+            }else{
+                System.out.println("NO");
+            }
+
+        }
+    }
+}
