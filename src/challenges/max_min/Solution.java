cec25f0bc7b5583fd1a227747e42b7ab18b79aee
Zhenyi Luo <zhenyiluo@gmail.com>
2015-03-09 22:15:10 -0400

    3345

---
+++ src/challenges/max_min/Solution.java
@@ -0,0 +1,34 @@
+package challenges.max_min;
+
+/**
+ * Created by Eric on 15-3-9.
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+// The part of the program involving reading from STDIN and writing to STDOUT has been provided by us.
+
+public class Solution {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(in.readLine());
+        int K = Integer.parseInt(in.readLine());
+        int[] list = new int[N];
+
+        for(int i = 0; i < N; i ++)
+            list[i] = Integer.parseInt(in.readLine());
+
+        int unfairness = Integer.MAX_VALUE;
+
+      /*
+       * Write your code here, to process numPackets N, numKids K, and the packets of candies
+       * Compute the ideal value for unfairness over here
+      */
+
+        System.out.println(unfairness);
+    }
+}
