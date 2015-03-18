447d11bfe8cb8d6e76e175829dd34274dea85bad
Zhenyi Luo <zhenyiluo@gmail.com>
2015-03-18 07:56:00 -0400

    688

---
+++ src/challenges/dynamicProgramming/fibonacci_modified/Solution.java
@@ -0,0 +1,31 @@
+package challenges.dynamicProgramming.fibonacci_modified;
+
+/**
+ * Created by Eric on 15-3-18.
+ */
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int n = sc.nextInt();
+        System.out.println(solve(a, b, n));
+    }
+    public static BigInteger solve(int a, int b, int n){
+        BigInteger aa = BigInteger.valueOf(a);
+        BigInteger bb = BigInteger.valueOf(b);
+        BigInteger cc = BigInteger.valueOf(0);
+        for(int i = 2; i< n; i++){
+            cc = bb.multiply(bb).add(aa) ;
+            aa = bb;
+            bb = cc;
+        }
+        return cc;
+    }
+}
