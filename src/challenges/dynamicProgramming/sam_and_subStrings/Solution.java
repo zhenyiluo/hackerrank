611e3d6140fede8db8d82f19c5ada8b0e30dbe0c
Zhenyi Luo <zhenyiluo@gmail.com>
2015-03-19 21:40:22 -0400

    536

---
+++ src/challenges/dynamicProgramming/sam_and_subStrings/Solution.java
@@ -0,0 +1,26 @@
+package challenges.dynamicProgramming.sam_and_subStrings;
+
+/**
+ * Created by Eric on 15-3-19.
+ */
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        System.out.println(solve(s));
+    }
+    public static long solve(String s){
+        final long mod = 1000000007;
+        long cur_sum = 0;
+        long total_sum = 0;
+        for(int i = 0; i < s.length(); i++){
+            cur_sum = (cur_sum * 10 + (s.charAt(i) - '0') * (i+1)) % mod;
+            total_sum = (total_sum + cur_sum) % mod;
+        }
+        return total_sum;
+    }
+}
